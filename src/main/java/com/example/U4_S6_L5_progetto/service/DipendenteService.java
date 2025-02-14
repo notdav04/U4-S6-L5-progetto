832e6dbbd85125fe53c9b400336a776019cfd63f
notdav04 <davide.alonzi26@gmail.com>
2025-02-14 18:13:36 +0100

    creazione primi 3 metodi per dipendente

--- src/main/java/com/example/U4_S6_L5_progetto/service/DipendenteService.java
+++ src/main/java/com/example/U4_S6_L5_progetto/service/DipendenteService.java
@@ -1,16 +1,54 @@
 package com.example.U4_S6_L5_progetto.service;
 
 import com.example.U4_S6_L5_progetto.entity.Dipendente;
+import com.example.U4_S6_L5_progetto.entity.Viaggio;
 import com.example.U4_S6_L5_progetto.payload.DipendenteDTO;
+import com.example.U4_S6_L5_progetto.payload.ViaggioDTO;
 import com.example.U4_S6_L5_progetto.repository.DipendenteDAORepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DipendenteService {
 
     @Autowired
     DipendenteDAORepository dipendenteDAO;
+
+    //metodi dao
+    //salvo dipendente( post )
+    public Long saveDipendente(DipendenteDTO dipendenteDTO){
+        Dipendente dipendente = fromDipendenteDTOToEntity(dipendenteDTO);
+        return dipendenteDAO.save(dipendente).getId();
+    }
+    //prendo tutti i dipendenti( get all )
+    public Page<DipendenteDTO> findAllDipendente(Pageable page) {
+        Page<Dipendente> listaDipendenti = dipendenteDAO.findAll(page);
+        List<DipendenteDTO> listaDipendentiDTO = new ArrayList<>();
+        for (Dipendente dipendente : listaDipendenti.getContent()) {
+            DipendenteDTO dipendenteDTO =fromDipendenteToDipendenteDTO(dipendente);
+                    listaDipendentiDTO.add(dipendenteDTO);
+        }
+        Page<DipendenteDTO> listaPage = new PageImpl<>(listaDipendentiDTO);
+        return listaPage;
+    }
+
+    //prendo un solo dipendente( get by id )
+    public DipendenteDTO findDipendenteById(long id){
+        Optional<Dipendente> dipendente = dipendenteDAO.findById(id);
+        if (dipendente.isPresent()){
+            DipendenteDTO dipendenteDTO = fromDipendenteToDipendenteDTO(dipendente.get());
+            return dipendenteDTO;
+        }else{
+            throw new RuntimeException("nessun dipendente trovato con l id richiesto")
+        }
+    }
 
     //travasi DTO
     public Dipendente fromDipendenteDTOToEntity(DipendenteDTO dipendenteDTO) {
